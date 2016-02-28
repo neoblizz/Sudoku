ae78f5fdc296a3f70b8a1354a85f42eb722178d9
ahtobin <ahtobin@ucdavis.edu>
2016-02-28 02:10:50 -0800

    Added Square struct
    Included ideas for how to use the parameters. Some may not be necessary depending on how we use others.

--- data.cuh
+++ data.cuh
@@ -14,3 +14,15 @@ struct CMD {
   bool gfx_output;
   Puzzle * unsolved;
 }; /* Stores command line arguments */
+
+struct Square {
+  int value; 		//not needed if we always check len(possValues)
+					//else set to 0 for all Squares and set when lock
+					//so we could really just skip the lock if just check =0
+
+	bool isLocked,		//may want to replace with int, so we can
+					//guess and keep track of what is locked/guessed
+
+	int possValues[9]	//for keeping track of what values this Square could be
+					//based on other locked Squares in local row/col/block
+};
