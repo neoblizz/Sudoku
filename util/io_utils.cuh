d264ab85a4c176c363fd03a20ce3404643c96ab7
[neoblizz] <[mosama94@uw.edu]>
2016-03-11 18:22:24 -0800

    Populated possValues during init.

--- util/io_utils.cuh
+++ util/io_utils.cuh
@@ -125,6 +125,7 @@ void input(int argc, char** argv, CommandLineArgs * build) {
         // cout << (*build).Puzzle[i].value;
         (*build).Puzzle[i].isLocked = -1;
     } else {
+        (*build).Puzzle[i].possValues = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         (*build).Puzzle[i].isLocked = 0;
     }
 
